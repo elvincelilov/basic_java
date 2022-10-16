a5295740bf362b1fc4b6a38d6cc53bd53fd30694
elvincelilov <celilovelvin11@gmail.com>
2022-10-16 10:41:22 +0400

    commit

--- src/main/java/az/java_lesson/exercise/FindWordinString.java
+++ src/main/java/az/java_lesson/exercise/FindWordinString.java
@@ -1,5 +1,25 @@
 package az.java_lesson.exercise;
 
+import java.util.Scanner;
+
 public class FindWordinString {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("String daxil edin : ");
+        String input = sc.nextLine();
+        countWordsUsingSplit(input);
+//        findword(word);
+    }
+
+//    public static void findword(String word) {
+//        String[] words = word.split(" ");
+//        System.out.println(words.length);
+//    }
+
+    public static void countWordsUsingSplit(String input) {
+
+        String[] words = input.split("\\s+");
+        System.out.println(words.length);
+    }
+
 }
