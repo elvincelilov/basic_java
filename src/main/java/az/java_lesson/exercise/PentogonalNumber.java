e45f101c514cf734469b510434641fc3d9163ac8
elvincelilov <celilovelvin11@gmail.com>
2022-10-17 09:59:51 +0400

    pentogonal

--- src/main/java/az/java_lesson/exercise/PentogonalNumber.java
+++ src/main/java/az/java_lesson/exercise/PentogonalNumber.java
@@ -1,9 +1,13 @@
 package az.java_lesson.exercise;
 
 public class PentogonalNumber {
-    public  static  void pentagonal(int a){
-        for (int i = a; i < 50; i++) {
-
+    public static void main(String[] args) {
+        int count = 1;
+        for(int i = 1; i <= 50; i++){
+            getPentagonalNumber(i);
         }
+    }
+    public static void getPentagonalNumber(int i) {
+        System.out.println((i * (3 * i - 1))/2);
     }
 }
